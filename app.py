9791c8a975bb7062f2d962f8b3221168b75f7974
Jason Benn <jasoncbenn@gmail.com>
2019-03-19 01:27:52 -0700

    Tie it all together :)

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import jsonify
+
 
 from main import main
 
@@ -12,12 +14,12 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
 
-@app.route('/api/generate', methods=['GET', 'POST'])
+@app.route('/api/generate', methods=['POST'])
 def generate():
     context = request.get_json(force=True)
     phrase = context.get('text')
@@ -25,4 +27,4 @@ def generate():
         generated = main(phrase)
     else:
         generated = "NOP U NEED TO SEND PHRASE - I AM SMART BUT NOT A MIND READER"
-    return "".join(generated)
+    return jsonify({"response": "".join(generated)})
